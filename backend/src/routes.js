284470c8471ee7371d1aaabde720716e4afabca7
Felipe Barros <felipetech@tuta.io>
2020-03-24 13:04:15 -0300

    chore (backend): Add route to create an "ONG" account

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,34 @@
 const routes = require('express').Router();
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
+});
+
+routes.post('/ongs', async (req, res) => {
+  try {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('hex');
+
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    });
+
+    return res.status(201).json({ id });
+  } catch (err) {
+    const error = {
+      message: "Couldn't create an account",
+    };
+    return res.json({ error });
+  }
 });
 
 module.exports = routes;
