e3ea355f99e0f15271da775d025b9e71db3bc300
Felipe Barros <felipetech@tuta.io>
2020-03-24 09:53:37 -0300

    chore (backend): Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 3333);
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World'  });
+});
+
+app.listen(app.get('PORT'), () => {
+  console.log(`Server running on *:${app.get('PORT')}`);
+});
